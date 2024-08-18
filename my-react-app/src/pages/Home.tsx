27fff758e580824ef91c92582c4fd4dc0eb41664
Potter <118960067+Potterpurpp@users.noreply.github.com>
2024-08-18 17:53:43 +0700

    Add styled

---
+++ my-react-app/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Social Enterprise</h1>
+      <p>
+        Our mission is to solve social issues like education, health,
+        environment, and poverty.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
