2b5da8716fffa72971bbb30726aeb8e2a9f91284
aomnachh4 <119651033+aomnachh4@users.noreply.github.com>
2024-09-26 23:52:23 +0700

    add portaom

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -2,23 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import StyledNavbar from "./components/Navbar";
-import ProjectsPage from "./pages/Project";
 import ContactPage from "./pages/contact";
-import FundraisingPage from "./pages/Fundraising";
 import KnowledgePage from "./pages/KnowlegdePage";
 import PotterPort from "./potter/PotterPort";
+import PortAom from "./aom/portaom";
+
 
 const App: React.FC = () => {
   return (
     <Router>
       <StyledNavbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/fundraising" element={<FundraisingPage />} />{" "}
+        <Route path="/" element={<Home />} />    
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/knowledge" element={<KnowledgePage />} />
         <Route path="/potterport" element={<PotterPort />} />
+        <Route path="/portaom" element={<PortAom />} />
       </Routes>
     </Router>
   );
---
+++ my-react-app/src/aom/PortAom.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PortAom = () => {
+
+  return (
+    <div>portaom</div>
+
+  )
+}
+
+export default PortAom
